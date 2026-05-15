2ee658ae874dff34a26e7290e918ad992e59b59a
EERAH-LEE <kor.si1229@gmail.com>
2026-05-15 15:43:24 +0900

    feat : 05.02 5323223

--- chapter05/02/lab02/modules/network/locals.tf
+++ chapter05/02/lab02/modules/network/locals.tf
@@ -8,6 +8,10 @@ locals {
     enable_dns_support = true
   }
 
+  natgw = {
+    name = "eerah"
+  }
+
   public_subnet = [
   {
     name = "public-a"
@@ -24,5 +28,19 @@ locals {
   }
   ]
 
+  private_subnet = [
+  {
+    name = "private-c"
 
+    cidr_block = "10.0.101.0/24"
+    availability_zone = "ap-northeast-2c"
+    map_public_ip_on_launch = false
+  },
+  {
+    name = "private-d"
+    cidr_block = "10.0.102.0/24"
+    availability_zone = "ap-northeast-2d"
+    map_public_ip_on_launch = false
+  }
+  ]
 }
